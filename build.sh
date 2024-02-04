d9508bda8e78ece63aa1db3475e701537468c514
Alevtina ("Allie") Verbovetskaya <alevtina.verbovetskaya@cuny.edu>
2024-02-03 19:03:06 -0500

    Changed location of output files

--- build.sh
+++ build.sh
@@ -5,19 +5,19 @@ SITES=(
   "bm"
 )
 
-rm -rf public
+rm -rf output
 
 for SITE in ${SITES[@]}; do
-  mkdir -p public/${SITE}/{pages,public}
+  mkdir -p output/${SITE}/{pages,public}
 
   for PAGE in pages/*; do
-    ./bin/tera --include-path=templates --template="${PAGE}" "configs/${SITE}.toml" --out "public/${SITE}/${PAGE}"
+    ./bin/tera --include-path=templates --template="${PAGE}" "configs/${SITE}.toml" --out "output/${SITE}/${PAGE}"
   done
 
-  mv public/${SITE}/pages/* public/${SITE}/
-  rm -rf public/${SITE}/pages
-  cp -R logos/${SITE}.png public/${SITE}/public/logo.png
-  cp -R favicons/${SITE}.ico public/${SITE}/public/favicon.ico
-  cp -R static/* public/${SITE}/public
-  zip -r public/${SITE}.zip public/${SITE}
+  mv output/${SITE}/pages/* output/${SITE}/
+  rm -rf output/${SITE}/pages
+  cp -R logos/${SITE}.png output/${SITE}/public/logo.png
+  cp -R favicons/${SITE}.ico output/${SITE}/public/favicon.ico
+  cp -R static/* output/${SITE}/public
+  zip -r output/${SITE}.zip output/${SITE}
 done
